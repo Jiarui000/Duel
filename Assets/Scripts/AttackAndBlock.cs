86d5ffbbec673e4e9b977ba7ae3386326c543f75
Jiarui000 <fengjiarui2014@gmail.com>
2018-08-30 13:30:50 +1000

    Initial commit

---
+++ Assets/Scripts/AttackAndBlock.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackAndBlock : MonoBehaviour
+{
+    #region Variables
+    public PlayerController player;
+    public GameObject enemy;
+    public int whichPlayer;
+    [Header("Stab")]
+    public GameObject stabCol;
+    public float stabForce = 40;
+    public float stabSlowdown;
+    public float stabPower;
+    public float stabDir;
+    [Header("Block")]
+    public GameObject blockCol;
+    public bool isBlocking;
+    public float blockSpeed = 2;
+    public float blockTimer, maxBlockTimer = 5;
+    [Header("Swipe")]
+    public GameObject swipeCol;
+
+
+    #endregion
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GetComponent<PlayerController>();
+        enemy = player.enemy;
+        stabCol = GameObject.Find("StabCol");
+        blockCol = GameObject.Find("BlockCol");
+        swipeCol = GameObject.Find("SwipeCol");
+        stabCol.active = false;
+        blockCol.active = false;
+        swipeCol.active = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        blockTimer -= Time.deltaTime;
+        #region Stab
+        Vector3 towardsEnemy = (transform.position - enemy.transform.position).normalized;
+        if (Input.GetButtonDown("Stab" + whichPlayer) && !Input.GetButton("Block" + whichPlayer) && !Input.GetButton("Swipe" + whichPlayer))
+        {
+            stabCol.active = true;
+            stabPower = stabForce;
+            stabDir = transform.rotation.y;
+            GetComponent<Rigidbody>().AddForce(new Vector3(towardsEnemy.x,0,0) * -stabForce, ForceMode.Impulse);
+        }
+        stabPower -= stabSlowdown;
+        if (stabPower < 0)
+        {
+            stabPower = 0;
+            stabCol.active = false;
+        }
+        #endregion
+        #region Block
+        if (Input.GetButton("Block" + whichPlayer) && stabPower <= 0 && blockTimer < 0)
+        {
+            isBlocking = true;
+        }
+        else
+        {
+            isBlocking = false;
+        }
+        if (isBlocking)
+        {
+            Block();
+        }
+        else
+        {
+            blockCol.active = false;
+            player.slowdownSpeed = 1;
+            
+        }
+        #endregion
+        #region Swipe
+        if (Input.GetButtonDown("Swipe" + whichPlayer) && !Input.GetButton("Stab" + whichPlayer) && !Input.GetButton("Block" + whichPlayer))
+        {
+            swipeCol.active = true;
+        }
+        else
+        {
+            swipeCol.active = false;
+        }
+        #endregion
+    }
+    void Block()
+    {
+        blockCol.active = true;
+        player.slowdownSpeed = blockSpeed;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Variables
+    [Header("Movement")]
+    public float moveSpeed = 2.5f;
+    public float jumpHeight = 4;
+    public float slowdownSpeed = 2;
+    public float knockbackForce = 10;
+    public bool grounded;
+    public Rigidbody rb;
+    Vector3 spawnPoint;
+    public AttackAndBlock attackScript;
+    public GameObject enemy;
+
+    #endregion
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        attackScript = GetComponent<AttackAndBlock>();
+        spawnPoint = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        #region GroundCheck
+        if (Physics.Raycast(transform.position, transform.TransformDirection(new Vector3(0, -1, 0)), 1.1f))
+        {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
+        #endregion
+        #region Movement
+        rb.velocity = (new Vector3((Input.GetAxisRaw("Horizontal" + attackScript.whichPlayer) * (moveSpeed * 100)) * Time.deltaTime / slowdownSpeed, rb.velocity.y, 0));
+        if (Input.GetButton("Jump" + attackScript.whichPlayer) && grounded)
+        {
+            rb.velocity = (new Vector3(rb.velocity.x, (jumpHeight * 100) * Time.deltaTime, 0));
+        }
+        #endregion
+        if (transform.position.y < -10) { transform.position = spawnPoint; }
+        transform.LookAt(new Vector3(enemy.transform.position.x, transform.position.y, 0));
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "StabCol" && other.tag == enemy.transform.tag && !attackScript.isBlocking)
+        {
+            Debug.Log("Player "+attackScript.whichPlayer+": has Died!");
+            transform.position = new Vector3(100, 10,0);
+        }
+        if (other.name == "StabCol" && other.tag == enemy.transform.tag && attackScript.isBlocking)
+        {
+            Vector3 knockBackDirection =  (transform.position  - enemy.transform.position).normalized;
+            enemy.GetComponent<Rigidbody>().AddForce(new Vector3(knockBackDirection.x,0,0) * -knockbackForce, ForceMode.Impulse);
+        }
+        if (other.name == "SwipeCol" && other.tag == enemy.transform.tag)
+        {
+            attackScript.isBlocking = false;
+            attackScript.blockTimer = attackScript.maxBlockTimer;
+        }
+    }
+}
